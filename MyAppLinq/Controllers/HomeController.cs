067f78a95a39eeb51e21ef3a406bfdc39c57bac6
cyrielletouton <cyrielle.t@hotmail.fr>
2022-01-30 15:44:58 +0100

    Ajoutez des fichiers projet.

---
+++ MyAppLinq/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyAppLinq.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Ma page à propos.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Ma page de contact.";
+
+            return View();
+        }
+    }
+}
